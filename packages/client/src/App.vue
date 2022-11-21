260dd354412d1b608d518a353274610848878ad8
hakuna <943383098@qq.com>
2022-11-21 18:12:11 +0800

    init

---
+++ packages/client/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>App</div>
+</template>
+
+<style lang="stylus" scoped></style>
---
+++ packages/client/src/main.ts
@@ -0,0 +1,5 @@
+import {createApp} from 'vue'
+import '@/assets/styles/reset.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
