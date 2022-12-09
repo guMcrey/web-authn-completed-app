30377d45ae0f132f5484e4a5c6657feb47f9a98e
hakuna <943383098@qq.com>
2022-12-09 12:13:43 +0800

    update loading

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,29 +1,16 @@
 <template>
-  <div v-if="loading" class="global-loading">
-    <img src="@/assets/images/loading.gif" alt="loading" />
-  </div>
-  <router-view v-else />
+  <router-view />
 </template>
 
 <script lang="ts" setup>
 import {onMounted} from 'vue'
 import {useLoginUser} from '@/apis/useLogin'
 
-const {loading, fetchData} = useLoginUser()
+const {fetchData} = useLoginUser()
 
 onMounted(() => {
+  const loadingEl = document.querySelector('.global-loading') as HTMLElement
+  loadingEl.style.display = 'none'
   fetchData()
 })
 </script>
-
-<style lang="stylus" scoped>
-.global-loading
-  background-color #FCFCFC
-  display flex
-  align-items center
-  justify-content center
-  height 100vh
-  img
-    max-width 32%
-    max-height auto
-</style>
