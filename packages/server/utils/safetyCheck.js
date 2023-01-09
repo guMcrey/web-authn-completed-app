9bed478021bdcd30794815cd0da30ffd3462617d
hakuna <943383098@qq.com>
2023-01-09 18:24:38 +0800

    fix is-login 401

--- packages/server/utils/safetyCheck.js
+++ packages/server/utils/safetyCheck.js
@@ -1,6 +1,6 @@
 const sessionCheck = (req, res, next) => {
     if (req.session['signed-in'] !== 'yes') {
-        return res.status(401).send({
+        return res.status(200).send({
             code: 401,
             data: {},
             message: 'not signed in.'
